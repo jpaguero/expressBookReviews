5700012dda0a376453e3a369ff5a2fe97b7648af
jpaguero <jaguero@truelogic.io>
2025-03-24 11:57:12 -0600

    Tasks 1 to 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,33 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    return res.status(300).json(JSON.stringify(books));
+})
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let filteredBooks = {}
+  Object.keys(books).map(book => {
+    if(book === isbn) {
+      filteredBooks[isbn] = books[isbn]
+    }
+  });
+  return res.status(300).json(JSON.stringify(filteredBooks));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let autor = req.params.author;
+  filteredBooks = Object.values(books).filter(book => book.author === autor);
+  return res.status(300).json(JSON.stringify(filteredBooks));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  filteredBooks = Object.values(books).filter(book => book.title === title);
+  return res.status(300).json(JSON.stringify(filteredBooks));
 });
 
 //  Get book review
